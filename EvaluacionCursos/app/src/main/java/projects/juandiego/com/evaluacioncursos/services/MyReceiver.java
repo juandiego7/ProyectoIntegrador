d5e4db7340f309f7e005ba9e9677e5285960f9a5
raulms10 <rantonio.martinez@udea.edu.co>
2017-05-10 13:40:24 -0500

    Correccion Servicio Web preguntas -Android

--- EvaluacionCursos/app/src/main/java/projects/juandiego/com/evaluacioncursos/services/MyReceiver.java
+++ EvaluacionCursos/app/src/main/java/projects/juandiego/com/evaluacioncursos/services/MyReceiver.java
@@ -12,7 +12,7 @@ import projects.juandiego.com.evaluacioncursos.MainActivity;
 public class MyReceiver extends BroadcastReceiver {
 
     @Override
-    public void onReceive(Context contegit xt, Intent intent) {
+    public void onReceive(Context context, Intent intent) {
         Log.d("onHandleIntent", "Recibido");
         Toast.makeText(MainActivity.getContext(), "Recibido "+intent.getAction(), Toast.LENGTH_SHORT).show();
         Intent intent2 = new Intent(MainActivity.getContext(), EvaluarActivity.class);
